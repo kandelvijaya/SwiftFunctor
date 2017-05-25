5c8b064e326315e5fe801c0d2814f9f8942802c0
vkandel <vijaya.prakash.kandel@zalando.de>
2017-05-25 18:15:30 +0200

    Added extensive documentation for the functions annd Functor type

--- Functor.playground/Sources/Functor.swift
+++ Functor.playground/Sources/Functor.swift
@@ -13,19 +13,37 @@ import Foundation
  Functor nature not by compliance to type but by mere informal method.
  
  
- For example: 
+ For example:
+ We are looking semantically for a function like such.
  ```
  struct Maybe<A> {
     func fmap(_ by: (A) -> B) -> Maybe<B> {...}
  }
  ```
+ 
+ Due to the limitation on Self type and Protocol<X>; we cannot model 
+ functor with the same syntax as above. We can however semantically 
+ perform the same operation. The call site will look like this:
+ ```
+ let intMaybe = Maybe<Int>(with: 12)
+ let c: Maybe<String> = intMaybe.fmap{ "\($0)" }
+ c.value    // "24"
+ ```
+ 
+ NOTE: `let c: Maybe<String>` is used to store returned Functor into a 
+ complete representable type. Functor is a Generic Type.
  */
 public protocol Functor {
 
     associatedtype A
 
-    // How to get -> Maybe<U>
-    // this gets Functor which needs to be downcasted to get proper type
+    /**
+     Maps over the current functor by applying function `by` to produce another functor.
+
+     - parameter by: Takes function `(A) -> B`
+     - description `A`: type of each item in current Functor.
+     - description `B`: type of each item that will be in the return Functor.
+     */
     func fmap<F: Functor, B>(_ by: ((A) -> B)) -> F where F.A == B
     
 }
@@ -38,17 +56,5 @@ protocol Functor2 {
     // This returns Functor2 which doesnot constraint the internal items to be of type B
     // this is bad by design
     func fmap<B, F: Functor2>(_ by: ((Self.A) -> B)) -> F
-
-}
-
-
-// this is the same as the first one
-protocol Functor3 {
-
-    associatedtype A
-
-    // This is essentially the same as the first one
-    // How can we get the proper type Maybe<B>
-    func fmap<B, U>(_ by: (Self.A) -> B) -> U where U : Functor3, U.A == B
 
 }
